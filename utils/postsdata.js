f669dc776a43e48c5051ce2de6330b25836ef6c7
Rickshen1 <37325295+Rickshen1@users.noreply.github.com>
2018-04-12 18:13:42 +0800

    add:newsList module
    添加：新增了“快报”模块，用以展示最新新闻 @shenjh 20180412

---
+++ utils/postsdata.js
@@ -0,0 +1,75 @@
+var local_database = [
+
+  {
+
+    date: "2018-04-03",
+
+    title: "中国移动获发FDD牌照：合法开启低频重耕 利国利企利民",
+
+    imgSrc: "../img/fdd.jpg",
+
+    avatar: "../img/myinfo.jpg",
+
+    content: "工业和信息化部依照法定程序，严格审核，于今天向中国移动颁发了基础电信业务经营许可证，批准中国移动经营LTE/第四代数字蜂窝移动通信业务（LTE FDD）。",
+
+    reading: "92",
+
+    collection: "65",
+
+    view_img: "../img/sc.jpg",
+
+    collect_img: "../img/coffee.png",
+
+  },
+
+  {
+
+    date: "2018-04-03",
+
+    title: "中国移动获发FDD牌照：合法开启低频重耕 利国利企利民",
+
+    imgSrc: "../img/fdd.jpg",
+
+    avatar: "../img/myinfo.jpg",
+
+    content: "工业和信息化部依照法定程序，严格审核，于今天向中国移动颁发了基础电信业务经营许可证，批准中国移动经营LTE/第四代数字蜂窝移动通信业务（LTE FDD）。",
+
+    reading: "88",
+
+    collection: "66",
+
+    view_img: "../img/sc.jpg",
+
+    collect_img: "../img/coffee.png",
+
+  },
+
+  {
+    date: "2018-04-03",
+
+    title: "中国移动获发FDD牌照：合法开启低频重耕 利国利企利民",
+
+    imgSrc: "../img/fdd.jpg",
+
+    avatar: "../img/myinfo.jpg",
+
+    content: "工业和信息化部依照法定程序，严格审核，于今天向中国移动颁发了基础电信业务经营许可证，批准中国移动经营LTE/第四代数字蜂窝移动通信业务（LTE FDD）。",
+
+    reading: "123",
+
+    collection: "55",
+
+    view_img: "../img/sc.jpg",
+
+    collect_img: "../img/coffee.png",
+
+  }
+
+]
+
+
+module.exports = {
+
+  postList: local_database
+
+}
